404a48d219af8ed07e1e0fd2a65e4d61675207f3
Minehacker765 <fortniterosprospros@gmail.com>
2021-10-02 21:57:02 +0200

    Backups

--- modules/backup.py
+++ modules/backup.py
@@ -1,6 +1,6 @@
 from discord.ext.commands import Cog, Bot, command, Context
 from bot_api import _m_s
-from zipfile import ZipFile
+from shutil import make_archive
 from os import walk
 from os.path import basename
 from os import chdir
@@ -12,13 +12,7 @@ class Backup(Cog):
 
     @Cog.listener()
     async def on_connect(self):
-        chdir('backups')
-        open(f'backup {self.bot.started.day}-{self.bot.started.month}-{self.bot.started.year} {self.bot.started.hour}:{self.bot.started.minute}:{self.bot.started.second}.zip', 'w').close()
-        with ZipFile(f'backup {self.bot.started.day}-{self.bot.started.month}-{self.bot.started.year} {self.bot.started.hour}:{self.bot.started.minute}:{self.bot.started.second}.zip') as f:
-            for n, sf, m in walk('.'):
-                for nm in m:
-                    f.write(join(n, nm), basename(join(n, nm)))
-        chdir('..')
+        make_archive(f'backups/backup {self.bot.started.day}-{self.bot.started.month}-{self.bot.started.year} {self.bot.started.hour}:{self.bot.started.minute}:{self.bot.started.second}.zip', 'zip', '.')
 
 
 def setup(bot: Bot):
