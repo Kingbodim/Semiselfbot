e8f99d07cd73e6aa7b2ddbe79f43b11b364e56e9
Minehacker765 <fortniterosprospros@gmail.com>
2021-10-08 22:48:46 +0000

    Updating

--- pull.sh
+++ pull.sh
@@ -1,1 +1,5 @@
-[ -n "$(git diff --name-only origin/main)" ] && echo Update found, updating... && git pull https://Minehacker765:$GIT_TOKEN@github.com/Minehacker765/Semiselfbot &>/dev/null
+if [[ $(git diff --name-only origin/main) ]]
+then
+echo Update found, updating...
+git pull https://Minehacker765:$GIT_TOKEN@github.com/Minehacker765/Semiselfbot &>/dev/null
+fi
