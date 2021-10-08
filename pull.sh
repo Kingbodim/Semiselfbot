cb940ab1302043157cdd4cf2e0b42eea4b670569
Minehacker765 <fortniterosprospros@gmail.com>
2021-10-08 22:51:02 +0000

    Updating

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-if [[ $(git diff --name-only origin/main) ]]
+if [[ $(git diff --name-only | wc -c) -ne 0 ]]
 then
 echo Update found, updating...
 git pull https://Minehacker765:$GIT_TOKEN@github.com/Minehacker765/Semiselfbot &>/dev/null
