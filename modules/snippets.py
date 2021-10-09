9c341610883f7525e06be5e746ce63c22cd16702
Minehacker765 <fortniterosprospros@gmail.com>
2021-10-09 10:02:14 +0000

    Updating

--- modules/snippets.py
+++ modules/snippets.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Cog, Bot, group, Context
 from bot_api import _m_s
 from aiofiles import open as aopen
+from os import remove, listdir
 
 
 class Snippets(Cog):
@@ -17,11 +18,24 @@ class Snippets(Cog):
         async with aopen(f'snippets/{name}.txt', 'r') as f:
             await ctx.reply(await f.read())
 
-    @snippet.command(aliases=['set'])
+    @snippet.command(aliases=['set', 'add'])
     async def save(self, ctx: Context, name, *, content):
         async with aopen(f'snippets/{name}.txt', 'w') as f:
             await f.write(content)
             await ctx.reply(f'Sucessfully saved `{name}` snippet')
+
+    @snippet.command(aliases=['rm', 'remove', 'del'])
+    async def delete(self, ctx: Context, *, name):
+        try:
+            remove(f'snippets/{name}.txt')
+        except:
+            await ctx.reply(f"`{name}` snippet doesn't exist!")
+        else:
+            await ctx.reply(f'Sucessfully deleted `{name}` snippet')
+
+    @snippet.command(aliases=['ls', 'list'])
+    async def snippets(self, ctx: Context):
+        await ctx.reply('```'+'\n'.join(f.removesuffix('.txt') for f in listdir('snippets'))+'```')
 
 
 def setup(bot: Bot):
