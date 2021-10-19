5a4f60b9315889878332baa7af4a5cb5d4ad46af
Minehacker765 <fortniterosprospros@gmail.com>
2021-10-19 19:37:14 +0000

    Updating

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 if [[ $(git diff --name-only | wc -c) -ne 0 ]]
 then
 echo Update found, updating...
+git stash
 git pull https://Minehacker765:$GIT_TOKEN@github.com/Minehacker765/Semiselfbot &>/dev/null
 else
 echo Up to date!
